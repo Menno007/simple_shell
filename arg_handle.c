1f5a2ca646ae1c20f904ffc0fb902a42d6e38609
Menno007 <Menna.soft3@gmail.com>
2023-11-11 18:10:56 +0200

    free all

--- arg_handle.c
+++ arg_handle.c
@@ -7,13 +7,11 @@
 char **arg_handle(char *input)
 {
 	int arg_count = 0, i, input_len;
-	char **argv, *token, *tmp, *input_copy;
+	char **argv, *token, *tmp, *input_copy = NULL;
 
 	if (!input)
 		return (NULL);
-
 	input_len = strlen(input);
-
 	input_copy = malloc(sizeof(char) * (input_len + 1));
 	if (input_copy == NULL)
 		return (NULL);
@@ -26,6 +24,8 @@ char **arg_handle(char *input)
 		arg_count++;
 		tmp = strtok(NULL, " ");
 	}
+	free(input_copy);
+	input_copy = NULL;
 	argv = (char **)malloc((arg_count + 1) * sizeof(char *));
 	if (argv == NULL)
 	{
@@ -34,11 +34,17 @@ char **arg_handle(char *input)
 	}
 	strcpy(input_copy, input);
 	argv[0] = strtok(input_copy, " ");
-
-	for (i = 1; i <= arg_count; i++)
+	for (i = 0; i < arg_count && token != NULL; i++)
 	{
+		argv[i] = strdup(token);
 		token = strtok(NULL, " ");
-		argv[i] = token;
+		if (argv[i] == NULL)
+		{
+			free(input_copy);
+			free(*argv);
+			free(argv);
+			return (NULL);
+		}
 	}
 	argv[arg_count] = NULL;
 	strcpy(input, argv[0]);
