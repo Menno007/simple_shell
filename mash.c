03b387bd155cb314d7fb7792bdd205d005b3ea54
Menno007 <Menna.soft3@gmail.com>
2023-11-15 13:34:51 +0200

    test4

--- mash.c
+++ mash.c
@@ -42,7 +42,7 @@ int main(int __attribute__((unused))argc, char *argv[], char *envp[])
 				{
 					if (execve(check_access, argu, envp) == -1)
 					{
-						_printf("%s: %d: %s: not found\n", argv[0], counter, argu[0]);
+						_printf("%s: %d: %s: not found\n", argv[0], 1, argu[0]);
 						break;
 					}
 				}
@@ -50,7 +50,7 @@ int main(int __attribute__((unused))argc, char *argv[], char *envp[])
 					wait(NULL);
 			}
 			else
-				_printf("%s: %d: %s: not found\n", argv[0], counter, argu[0]);
+				_printf("%s: %d: %s: not found\n", argv[0], 1, argu[0]);
 
 		}
 		if (check_access && (strcmp(argu[0], check_access) == 1))
